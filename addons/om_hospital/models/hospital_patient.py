400942e89d941d9f9bcb777cb9c98136d11d40a8
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2025-10-17 16:21:28 +0000

    Add Chatter to Form View in Odoo

--- addons/om_hospital/models/hospital_patient.py
+++ addons/om_hospital/models/hospital_patient.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin'] 
     _rec_name = "name" # by default, the record name is 'name' field, but we can specify it explicitly
     
     name = fields.Char(string="Patient Name", required=True)
